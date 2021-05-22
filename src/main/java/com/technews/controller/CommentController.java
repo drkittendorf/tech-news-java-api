771c3b3ba9a2d4f0afb27e874ca2c6aecc9725ff
dale Kittendorf <drkittendorf@gmail.com>
2021-05-21 22:33:03 -0500

    contorller fix

--- src/main/java/com/technews/controller/CommentController.java
+++ src/main/java/com/technews/controller/CommentController.java
@@ -1,71 +1,47 @@
 package com.technews.controller;
 
-import com.technews.model.Post;
-import com.technews.model.User;
-import com.technews.repository.UserRepository;
-import com.technews.repository.VoteRepository;
+import com.technews.model.Comment;
+import com.technews.repository.CommentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-public class UserController {
+public class CommentController {
 
     @Autowired
-    UserRepository repository;
+    CommentRepository repository;
 
-    @Autowired
-    VoteRepository voteRepository;
-
-    @GetMapping("/api/users")
-    public List<User> getAllUsers() {
-        List<User> userList = repository.findAll();
-        for (User u : userList) {
-            List<Post> postList = u.getPosts();
-            for (Post p : postList) {
-                p.setVoteCount(voteRepository.countVotesByPostId(p.getId()));
-            }
-        }
-        return userList;
+    @GetMapping("/api/comments")
+    public List<Comment> getAllComments() {
+        return repository.findAll();
     }
 
-    @GetMapping("/api/users/{id}")
-    public User getUserById(@PathVariable Integer id) {
-        User returnUser = repository.getOne(id);
-        List<Post> postList = returnUser.getPosts();
-        for (Post p : postList) {
-            p.setVoteCount(voteRepository.countVotesByPostId(p.getId()));
-        }
 
-        return returnUser;
+    @GetMapping("/api/comments/{id}")
+    public Comment getComment(@PathVariable int id) {
+        return repository.getOne(id);
     }
 
-    @PostMapping("/api/users")
-    public User addUser(@RequestBody User user) {
-        // Encrypt password
-        user.setPassword(BCrypt.hashpw(user.getPassword(), BCrypt.gensalt()));
-        repository.save(user);
-        return user;
+
+    @PostMapping("/api/comments")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Comment createComment(@RequestBody Comment comment) {
+        return repository.save(comment);
     }
 
-    @PutMapping("/api/users/{id}")
-    public User updateUser(@PathVariable int id, @RequestBody User user) {
-        User tempUser = repository.getOne(id);
 
-        if (!tempUser.equals(null)) {
-            user.setId(tempUser.getId());
-            repository.save(user);
-        }
-        return user;
+    @PutMapping("/api/updateComment")
+    public Comment updateComment(@RequestBody Comment comment) {
+        return repository.save(comment);
     }
 
-    @DeleteMapping("/api/users/{id}")
+
+    @DeleteMapping("/api/comments/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteUser(@PathVariable int id) {
+    public void deleteComment(@PathVariable int id) {
         repository.deleteById(id);
     }
-
 }
